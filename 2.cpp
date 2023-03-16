470ad45892b10dfa1020aac91bb4eba1b05a6eac
vende11s <74505994+vende11s@users.noreply.github.com>
2023-03-16 11:37:43 +0100

    Create 2.cpp

---
+++ 2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int n;
+    cin>>n;
+
+    string s;
+    getline(cin, s);
+
+    while(n--){
+        int a,b;
+        cin>>a>>b;
+
+        cout<<a+b<<endl;
+    }
+
+}
